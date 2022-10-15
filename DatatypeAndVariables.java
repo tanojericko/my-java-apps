06c52a99ea0b58426fe88c4c3d158f4cd2e2e128
tanojericko <tanojericko@icloud.com>
2022-10-16 07:35:02 +0800

    First Commit lol

---
+++ DatatypeAndVariables.java
@@ -0,0 +1,15 @@
+class DataTypesAndVariables {
+    public static void main(String[] args) {
+
+        String name = "Jericko James Tano";
+        int age = 19;
+        char bloodtype = 'O';
+        String address = "San Pablo, Sto. Domingo, Ilocos Sur";
+
+        System.out.println("Hi, my name is "+name);
+        System.out.println("I am "+age + " years old");
+        System.out.println("My blood type is "+bloodtype + " sample only");
+        System.out.println("From "+address);
+
+    }
+}
